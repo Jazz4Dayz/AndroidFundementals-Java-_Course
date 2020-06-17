694edd178baf2efc1ffbdbe749b68ee5b1d7f9ea
Jazz4Dayz <33031151+Jazz4Dayz@users.noreply.github.com>
2020-06-16 20:59:33 -0400

    added back_to_top button under article

--- app/src/main/java/com/example/scrollingtext/MainActivity.java
+++ app/src/main/java/com/example/scrollingtext/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.scrollingtext;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ScrollView;
 
 public class MainActivity extends AppCompatActivity {
+    ScrollView scrollView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        scrollView = findViewById(R.id.scrollingView);
+    }
+
+    public void toTop(View view) {
+        scrollView.scrollTo(0,0);
     }
 }
