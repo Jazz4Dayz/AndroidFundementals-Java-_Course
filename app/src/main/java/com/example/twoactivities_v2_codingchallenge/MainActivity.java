adfd78c2ca5e7d7b587fdef50633cd1fed609698
Jazz4Dayz <33031151+Jazz4Dayz@users.noreply.github.com>
2020-06-21 23:28:04 -0400

    main activity sends conversations to second activity

--- app/src/main/java/com/example/twoactivities_v2_codingchallenge/MainActivity.java
+++ app/src/main/java/com/example/twoactivities_v2_codingchallenge/MainActivity.java
@@ -2,10 +2,14 @@ package com.example.twoactivities_v2_codingchallenge;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private static String convo;
+    public static final String EXTRA_MESSAGE = "com.example.android.twoactivities_v2_codingchallenge.extra.MESSAGE";
+    public static final int TEXT_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,16 +17,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void getConversation(View view) {
+    public void launchSecondActivity(View view) {
+        Intent intent = new Intent(this, SecondActivity.class);
+        String conversation;
+
         switch (view.getId()){
-            case R.id.button_conversation_1
-
+            case R.id.button_conversation_1:
+                conversation = getResources().getString(R.string.conversation_1);
+                intent.putExtra(EXTRA_MESSAGE, conversation);
+                startActivityForResult(intent, TEXT_REQUEST);
                 break;
-            case R.id.button_conversation_2
-
+            case R.id.button_conversation_2:
+                conversation = getResources().getString(R.string.conversation_2);
+                intent.putExtra(EXTRA_MESSAGE, conversation);
+                startActivityForResult(intent, TEXT_REQUEST);
                 break;
-            case R.id.button_conversation_3
-
+            case R.id.button_conversation_3:
+                conversation = getResources().getString(R.string.conversation_3);
+                intent.putExtra(EXTRA_MESSAGE, conversation);
+                startActivityForResult(intent, TEXT_REQUEST);
                 break;
         }
     }
